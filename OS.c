82d486e1c89f45d03c6bd768f5a93f9b6599861e
JiaomaWHU <jiaomajiaoma@hotmail.com>
2020-02-20 21:02:54 -0600

    finish SW1 work

--- OS.c
+++ OS.c
@@ -43,6 +43,7 @@ uint32_t ElapsedTimerCounter;
 struct tcb{
   int32_t *sp;       // pointer to stack (valid for threads not running
 	struct tcb *next;  // linked-list pointer
+	struct tcb *prior;  // linked-list pointer
   uint32_t id;				 // id for the current thread
 	uint32_t sleepState;		 // sleep state of the thread, used for suspension
 	uint32_t priority;	 // the priority of the current thread (lab 3)
@@ -56,6 +57,8 @@ tcbType *TailPt = NULL;
 
 int32_t Stacks[NUMTHREAD][STACKSIZE];
 uint32_t THREAD_ID = 0;	// THREAD_ID assign to threads
+
+void (*UserSW1Task)(void);   // user function
 
 /*------------------------------------------------------------------------------
   Systick Interrupt Handler
@@ -260,7 +263,7 @@ uint32_t OS_Id(void){
 int OS_AddPeriodicThread(void(*task)(void), 
    uint32_t period, uint32_t priority){
   // put Lab 2 (and beyond) solution here
-  Timer4A_Init(task, period * 1000, priority); // period ms sampling
+  Timer4A_Init(task, period * 80000, priority); // period per ms sampling
   return 0; // replace this line with solution
 };
 
@@ -269,11 +272,13 @@ int OS_AddPeriodicThread(void(*task)(void),
 
 
 /*----------------------------------------------------------------------------
-  PF1 Interrupt Handler
+  PF1/4? Interrupt Handler
  *----------------------------------------------------------------------------*/
 void GPIOPortF_Handler(void){
- 
+	GPIO_PORTF_ICR_R = 0x10;      // acknowledge flag4
+	(*UserSW1Task)();               // execute user task
 }
+
 //******** OS_AddSW1Task *************** 
 // add a background task to run whenever the SW1 (PF4) button is pushed
 // Inputs: pointer to a void/void background function
@@ -289,8 +294,23 @@ void GPIOPortF_Handler(void){
 //           determines the relative priority of these four threads
 int OS_AddSW1Task(void(*task)(void), uint32_t priority){
   // put Lab 2 (and beyond) solution here
- 
-  return 0; // replace this line with solution
+	UserSW1Task = task; // user function
+  SYSCTL_RCGCGPIO_R |= 0x00000020; // (a) activate clock for port F
+//  FallingEdges = 0;             // (b) initialize counter
+  GPIO_PORTF_DIR_R &= ~0x10;    // (c) make PF4 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x10;  //     disable alt funct on PF4
+  GPIO_PORTF_DEN_R |= 0x10;     //     enable digital I/O on PF4   
+  GPIO_PORTF_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
+  GPIO_PORTF_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x10;     //     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x10;     // (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x10;    //     PF4 is not both edges
+  GPIO_PORTF_IEV_R &= ~0x10;    //     PF4 falling edge event
+  GPIO_PORTF_ICR_R = 0x10;      // (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x10;      // (f) arm interrupt on PF4 *** No IME bit as mentioned in Book ***
+  NVIC_PRI7_R = (NVIC_PRI7_R&0xFF00FFFF)|(priority<<21); // (g) user set priority
+  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
+  return 1; // replace this line with solution
 };
 
 //******** OS_AddSW2Task *************** 
